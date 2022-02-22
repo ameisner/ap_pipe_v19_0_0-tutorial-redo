6417aca6203b0ab81ff3296d8e206151a0d3b1b3
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-02-22 11:42:26 -0700

    minimal version of processCcd that skips astrometric and photometric calibration

--- processCcd.sh
+++ processCcd.sh
@@ -1,4 +1,7 @@
 eups list lsst_distrib
 
 rm -rf DATA/rerun/processCcdOutputs
-processCcd.py DATA --calib DATA/CALIB --rerun processCcdOutputs --id visit=412321 ccdnum=42 filter=g --longlog &> processCcd.log &
+#processCcd.py DATA --calib DATA/CALIB --rerun processCcdOutputs --id visit=412321 ccdnum=42 filter=g --longlog &> processCcd.log &
+
+# config options based on: https://community.lsst.org/t/processccd-with-obs-decam-no-locations-for-get/3474
+processCcd.py DATA --calib DATA/CALIB --rerun processCcdOutputs --id visit=412321 ccdnum=42 filter=g --longlog --config calibrate.doPhotoCal=False calibrate.doAstrometry=False &> processCcd.log &
